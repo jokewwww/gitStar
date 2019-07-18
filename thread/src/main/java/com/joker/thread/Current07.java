8fde2f01884521f2732f4d582b6b8d4142c96a0b
用joker <2101418065@qq.com>
2019-07-18 20:00:04 +0800

    AtomicXXX 底层c语言,效率相对更高

---
+++ thread/src/main/java/com/joker/thread/Current07.java
@@ -0,0 +1,49 @@
+package com.joker.thread;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * AtomicXXX
+ * 原子操作类型:其中的每个方法都是源自操作,可以保证线程安全
+ */
+public class Current07 {
+
+  AtomicInteger count = new AtomicInteger(0);
+
+  void m() {
+    for (int i = 0; i < 10000; i++) {
+      count.incrementAndGet();
+    }
+  }
+
+  public static void main(String[] args) {
+      final Current07 c=new Current07();
+
+      List<Thread> threads=new ArrayList();
+
+    for (int i = 0; i < 10; i++){
+      threads.add(new Thread(new Runnable() {
+          public void run() {
+              c.m();
+          }
+      }));
+    }
+
+    for (Thread thread :threads ) {
+        thread.start();
+    }
+
+    for (Thread thread :threads ) {
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+      System.out.println(c.count.intValue());
+
+  }
+}
