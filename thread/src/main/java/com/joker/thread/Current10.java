b65c15ac29cd5336ee55d20dcb91652cd608af05
用joker <2101418065@qq.com>
2019-07-19 19:48:32 +0800

    CountDownLatch门闩模型可以避免锁的效率低下问题

--- thread/src/main/java/com/joker/thread/Current10.java
+++ thread/src/main/java/com/joker/thread/Current10.java
@@ -1,49 +1,56 @@
 package com.joker.thread;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 /**
- * 常量问题
- * 在定义同步代码块时,不要使用常量对象作为锁对象
+ * 门闩-CountDownLatch
+ * 可以和锁混合使用,或代替锁的功能
+ * 在门闩未完全开放之前等待,当门闩完全开放后执行
+ * 可以避免锁的效率低下问题
  */
 public class Current10 {
 
-    String s1="hello";
-    String s2=new String("hello");
-    Integer i1=1;
-    Integer i2=1;
+  CountDownLatch latch = new CountDownLatch(5);
 
-    void m1(){
-        synchronized (s1){
-            System.out.println("m1() execute");
-            while (true){
-
-            }
-        }
-
+  void m1() {
+    try {
+      latch.await(); // 等待门闩开放
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+    System.out.println("m1() method excute");
+  }
 
-    void m2(){
-        synchronized (s2){
-            System.out.println("m2() execute");
-            while (true){
-
-            }
-        }
+  void m2() {
+    for (int i = 0; i < 10; i++) {
+      if (latch.getCount() != 0) {
+        System.out.println("latch count:" + latch.getCount());
+        latch.countDown(); // 减门闩上的锁
+      }
+      /*try {
+        TimeUnit.SECONDS.sleep(500);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }*/
+      System.out.println("m2() method:"+i);
     }
+  }
 
   public static void main(String[] args) {
     final Current10 c=new Current10();
     new Thread(new Runnable() {
-        @Override
-        public void run() {
-            c.m1();
-        }
+      @Override
+      public void run() {
+        c.m1();
+      }
     }).start();
 
     new Thread(new Runnable() {
-        @Override
-        public void run() {
-            c.m2();
-        }
+      @Override
+      public void run() {
+        c.m2();
+      }
     }).start();
   }
 }
