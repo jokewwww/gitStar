624ecdb3a191808993ce89a1abe43be5b086662b
用joker <2101418065@qq.com>
2019-07-23 11:14:55 +0800

    多线程模拟生产者和消费者 Condition模式

--- thread/src/main/java/com/joker/container/Container02.java
+++ thread/src/main/java/com/joker/container/Container02.java
@@ -1,6 +1,7 @@
 package com.joker.container;
 
 import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -23,9 +24,75 @@ public class Container02<E> {
     public void put(E e){
         lock.lock();
         try{
-
+            while (list.size()==MAX){
+                System.out.println(Thread.currentThread().getName()+"等待....");
+                //进入等待队列.释放锁标记
+                //借助条件进入等待队列
+                producer.await();
+            }
+            System.out.println(Thread.currentThread().getName()+"put ...");
+            list.add(e);
+            count++;
+            consumer.signalAll();//借助条件唤醒所有消费者
         }catch (Exception ex){
             ex.printStackTrace();
+        }finally{
+            lock.unlock();
+        }
+    }
+
+    public E get(){
+        E e=null;
+        lock.lock();
+        try{
+            while (list.size()==0){
+                System.out.println(Thread.currentThread().getName()+"等待...");
+                consumer.await();
+            }
+            System.out.println(Thread.currentThread().getName()+"get...");
+            e=list.removeFirst();
+            count--;
+            consumer.signalAll();
+
+
+        }catch (Exception ec){
+            ec.printStackTrace();
+        }finally{
+            lock.unlock();
+        }
+        return e;
+    }
+
+    public static void main(String[] args){
+        final Container02<String> c=new Container02();
+        for(int i = 0; i <10 ; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for(int j = 0; j <5; j++) {
+                        System.out.println("获取容器元素"+j);
+                        System.out.println(c.get());
+                    }
+                }
+            },"consumer"+i).start();
+        }
+
+        try{
+            TimeUnit.SECONDS.sleep(2);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        for(int i = 0; i <2; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for(int j = 0; j <25; j++) {
+                        c.put("consumer value"+j);
+                    }
+                }
+            },"producer"+i).start();
+
         }
     }
 }
